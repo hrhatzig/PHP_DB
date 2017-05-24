3a85951f2886663b6cac2f2eb8b3531ce3fd442c
Chrysostomos Chatzigeorgiou <hrhatzig@gmail.com>
2017-05-25 01:26:04 +0300

    One 'slow' test, without HTML.

--- se2.php
+++ se2.php
@@ -14,19 +14,24 @@ if (!$conn) {
 	echo ".<br/><br/><br/>";
 }
 
-$sql = "SELECT * FROM FLINES";
+$sql = "SELECT FDOC, LMAST FROM FDOC WHERE FSERIES='7090'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "FLINES: " . $row["FLINES"]. "<br>";
-        echo "FDOC: " . $row["FDOC"]. "<br>";
-        echo "SMAST: " . $row["SMAST"]. "<br>";
-        echo "QTY1: " . $row["QTY1"]. "<br>";
-        echo "COMMENTS: " . $row["COMMENTS"]. "<br>";
-        echo "U_STATE: " . $row["U_STATE"]. "<br>";
-        echo "<br/><br/>";
+       	echo "<br/><br/><br/><br/>";
+    	echo "FDOC: " . $row["FDOC"]. "<br>";
+        echo "LMAST: " . $row["LMAST"]. "<br>";
+		$sql2 = "SELECT FLINES, QTY1, COMMENTS, SMAST FROM FLINES WHERE FDOC='".$row["FDOC"]."'";
+		$result2 = mysqli_query($conn, $sql2);
+	    while($row2 = mysqli_fetch_assoc($result2)) {
+	    	echo "FLINES: " . $row2["FLINES"]. "<br>";
+	    	echo "COMMENTS: " . $row2["COMMENTS"]. "<br>";
+	    	echo "QTY1: " . $row2["QTY1"]. "<br>";
+	    	echo "SMAST: " . $row2["SMAST"]. "<br>";
+	    	// echo "<br/><br/>";
+        }
     }
 } else {
     echo "0 results";
